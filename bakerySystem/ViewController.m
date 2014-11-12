6fabda701680ed2eca9978ca6c3fe3f93866f2c2
RenChenLD <chenren1992@gmail.com>
2014-11-11 22:18:20 -0600

    Initial Commit

---
+++ bakerySystem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  bakerySystem
+//
+//  Created by 陈仁 on 11/11/14.
+//  Copyright (c) 2014 陈仁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ bakerySystem/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bakerySystem
+//
+//  Created by 陈仁 on 11/11/14.
+//  Copyright (c) 2014 陈仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
